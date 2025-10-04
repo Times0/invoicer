174dd1d854ac666e05952ba85ad288ff834bbfba
Your Name <spotopaz@gmail.com>
2025-10-04 22:28:49 +0100

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@tanstack/react-router";
+
+export default function Header() {
+	return (
+		<header className="">
+			<h1 className="">
+				<Link to="/">Logo</Link>
+			</h1>
+		</header>
+	);
+}
